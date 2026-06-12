f7051ab4f3c2d96c48dac0ddaee5e2bcebf5d242
4mustafa454 <4mustafa454@gmail.com>
2026-06-11 23:11:11 -0500

    Day 1 js

---
+++ index.js
@@ -0,0 +1,13 @@
+//console.log(`Hello`);
+//console.log(`I like Apples`);
+
+//window.alert(`This is an alert!`);
+//window.alert(`I like oranges!`);
+
+//This is a comment
+document.getElementById("myH1").textContent=`Hello`;
+document.getElementById("myP").textContent = `I like Apple Juice`;
+
+/*
+  This is a comment
+*/
